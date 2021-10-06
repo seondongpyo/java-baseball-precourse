1845670a5732640167b5c36bbd292918e137afd1
seondongpyo <seondongpyo@gmail.com>
2021-10-06 14:08:05 +0900

    refactor: 코딩 컨벤션 준수를 위한 게임 결과 출력 로직 수정

--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -4,23 +4,31 @@ import baseball.domain.BaseballResult;
 
 public class ResultView {
 
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
+    private static final String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
+
     private ResultView() {
     }
 
     public static void show(BaseballResult result) {
         if (result.isNothing()) {
-            System.out.println("낫싱");
+            System.out.println(NOTHING);
             return;
         }
+        printStrikeBallCount(result);
         if (result.isFinish()) {
-            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 끝");
-            return;
+            System.out.println(GAME_OVER);
         }
+    }
+
+    private static void printStrikeBallCount(BaseballResult result) {
         if (result.hasStrike()) {
-            System.out.print(result.strikeCount() + "스트라이크 ");
+            System.out.printf("%d%s ", result.strikeCount(), STRIKE);
         }
         if (result.hasBall()) {
-            System.out.print(result.ballCount() + "볼");
+            System.out.printf("%d%s", result.ballCount(), BALL);
         }
         System.out.println();
     }
