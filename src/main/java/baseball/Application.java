9c66720189cbdd596fa022095bd3b5db44d7334a
seondongpyo <seondongpyo@gmail.com>
2021-10-06 10:17:31 +0900

    feat: 컴퓨터는 새로운 게임 시작 시 중복되지 않은 임의의 숫자 3개를 선택한다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import baseball.domain.BaseballNumber;
+import baseball.domain.BaseballPosition;
+import baseball.domain.Baseballs;
+import nextstep.utils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+        List<Integer> computerBaseballNumbers = createRandomBaseballNumbers();
+        Baseballs computerBaseballs = new Baseballs(computerBaseballNumbers);
     }
+
+    private static List<Integer> createRandomBaseballNumbers() {
+        List<Integer> baseballNumbers = new ArrayList<>();
+        while (baseballNumbers.size() < BaseballPosition.MAX_VALUE) {
+            int randomNumber = Randoms.pickNumberInRange(BaseballNumber.MIN_VALUE, BaseballNumber.MAX_VALUE);
+            addNumberIfNotDuplicate(baseballNumbers, randomNumber);
+        }
+        return baseballNumbers;
+    }
+
+    private static void addNumberIfNotDuplicate(List<Integer> baseballNumbers, int number) {
+        if (baseballNumbers.contains(number)) {
+            return;
+        }
+        baseballNumbers.add(number);
+    }
+
 }
